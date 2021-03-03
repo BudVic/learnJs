b8ac5a116a14ca3526f303988e0026e675e61cd8
vbudko <and-second@ya.ru>
2021-03-03 18:34:12 +0300

    learn 40

---
+++ script.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('.btn');
+function myAnimation() {
+  const element = document.querySelector('.box');
+  let pos = 0;
+  const id = setInterval(frame, 10);
+  function frame() {
+    if (pos === 300) {
+      clearInterval();
+    } else {
+      pos++;
+      element.style.top = pos + 'px';
+      element.style.left = pos + 'px';
+    }
+  }
+}
+btn.addEventListener('click', myAnimation);
+let num = [];
+let start = new Date();
+for (let i = 0; i < 1000000; i++) {
+  num[i] = i ** 3;
+}
+let end = new Date();
+
+alert(`${end - start} ms`);
+console.log(num); 
